08bdb2779609550616b52f5e798c0acfa98f5e21
BHAGIRATH <bhagirathbaraiya.dev@gmail.com>
2025-07-19 19:11:05 +0530

    changes in file structure and updated student ui *do not merge with any branch till now*

--- student/include/footer.php
+++ student/include/footer.php
@@ -1,7 +1,5 @@
-	<div class="footer">
-		<div class="container">
-			 
-
-			<b class="copyright">&copy; 2019 CMS </b> All rights reserved.
-		</div>
+<div class="footer">
+	<div class="container">
+		<b class="copyright">&copy; 2025 </b> All rights reserved.
 	</div>
+</div>
